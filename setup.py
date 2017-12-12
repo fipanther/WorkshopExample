44af7e19a8873ede7ee82c0e0d708f83385790b3
fipanther <astrofipanther@gmail.com>
2017-12-12 13:22:32 +1100

    Changing names

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
-      author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      url='http://github.com/fipanther/WorkshopExample',
+      author='Fiona Panther',
+      author_email='fiona.panther@anu.edu.au',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
